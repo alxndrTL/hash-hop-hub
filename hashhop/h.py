f208698e4b2fd6e35bb64626595148d32ec5a315
Alexandre TL <alextorresleguet@icloud.com>
2024-09-01 21:35:59 +0200

    hashes in one go in torch + string

--- hashhop/h.py
+++ hashhop/h.py
@@ -2,7 +2,7 @@ import math
 import torch
 
 class HashHop:
-    def __init__(self, n_tokens, hash_len: int = 8, n_hops = 2, cot = True):
+    def __init__(self, n_tokens, hash_len = 8, n_hops = 2, cot = True, vocab_size=52):
         
         self.n_tokens = n_tokens
         self.hash_len = hash_len
@@ -11,14 +11,15 @@ class HashHop:
         #n_hashes_pairs = math.floor(self.n_tokens / n_tokens_in_pair)
 
         n_tokens_in_chain = n_tokens_in_pair * n_hops # todo : possibilité d'une chaine plus grande que n_hops ?
-        self.n_chains = self.n_tokens / n_tokens_in_chain
+        self.n_chains = math.floor(self.n_tokens / n_tokens_in_chain)
 
         self.n_hops = n_hops
         self.cot = cot
+        self.vocab_size = vocab_size
 
     def sample(self):
         # generate hashes
-        hashes = torch.randint(low=0, high=128, size=(self.n_hops, self.hash_len)) #self.n_chains)) # todo : 128=vocab_size
+        hashes = torch.randint(low=2, high=2+self.vocab_size, size=(self.n_chains, self.n_hops+1, self.hash_len))
 
-        A = torch.cat([hashes[:-1, :], torch.zeros(self.n_hops-1, 1, dtype=torch.long), hashes[1:, :], torch.ones(self.n_hops-1, 1, dtype=torch.long)], dim=1)
+        A = torch.cat([hashes[:, :-1, :], torch.zeros(self.n_chains, self.n_hops, 1, dtype=torch.long), hashes[:, 1:, :], torch.ones(self.n_chains, self.n_hops, 1, dtype=torch.long)], dim=2)
         return A.flatten()
